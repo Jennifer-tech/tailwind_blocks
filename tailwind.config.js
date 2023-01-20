cbc8b512fc914fb435efeeb1df349f81b82e8ecd
unknown <muofunanyajennifer1@gmail.com>
2023-01-20 22:54:21 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      "md": "768px"
+    }
+  },
+  plugins: [],
+}
